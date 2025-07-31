7cfb6195cfdcdb94104e038b5b8442590549e30c
TelisAnt <telhs99@hotmail.com>
2025-07-31 14:22:39 +0300

    Added symbols, uppercase

--- generator.py
+++ generator.py
@@ -3,9 +3,43 @@ import string
 
 def main():
     print("Lets create a strong password!")
+    while True:
+        length_input = input("Give password length (at least 15 characters): ")
+        try:
+            length = int(length_input)
+            if length >= 15:
+                break
+            else:
+                print("Password length must be at least 15 characters. Try again.")
+        except ValueError:
+            print("Please enter a valid number (digits only).")
 
-    length = input("Give password lenght (at least 10 characters): ")
-    uppercase = input("Include uppercase letters?[y/n]")
-    symbols = input("Include symbols? [y/n]:")
+    uppercase_input = input("Include uppercase letters?[y/n]")
+    lowercase = string.ascii_lowercase
 
-    alphabet = string.ascii_letters + string.digits #Characters that will be used
+    symbols_input = input("Include symbols? [y/n]:")
+    symbols = '-_*&^%$#@!'
+
+    alphabet = lowercase + string.digits #Characters that will be used (only lowercase at the beggining)
+        
+    if(uppercase_input == "y"):
+        uppercase = string.ascii_uppercase
+        alphabet += uppercase
+        if(symbols_input == "y"):
+            alphabet += symbols #append symbols if the user wants to
+            password = ''.join(secrets.choice(alphabet) for i in range(length))
+            print(password)
+        else:
+           password = ''.join(secrets.choice(alphabet) for i in range(length))
+           print(password)
+    else:
+        if(symbols_input == "y"):
+            alphabet += symbols #append symbols if the user wants to
+            password = ''.join(secrets.choice(alphabet) for i in range(length))
+            print(password)
+        else:
+            password = ''.join(secrets.choice(alphabet) for i in range(length))
+            print(password)
+
+
+main()
